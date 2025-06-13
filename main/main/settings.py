46dab9a7bfec41ea551219b5648294054b4cccb8
Jamerzbatman <jamiethetec@gmail.com>
2025-06-13 15:50:27 -0700

    added stuff

--- main/main/settings.py
+++ main/main/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-
+import os, dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,13 +31,26 @@ if DEBUG:
         "localhost",
         "192.168.1.85", 
     ]
-
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 else:
 
     ALLOWED_HOSTS = [
         "snackranger.com",
         "www.snackranger.com",
     ]
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ["DATABASE_URL"],
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://snackranger.com",
